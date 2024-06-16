e524bb5ff18b33423fc14954d5fed836a91e7558
Samuel Xavier <sancrisxa@gmail.com>
2024-06-16 17:40:22 -0300

    Create entity

--- src/main/java/br/com/sancrisxa/cryptography/service/CryptoService.java
+++ src/main/java/br/com/sancrisxa/cryptography/service/CryptoService.java
@@ -1,5 +1,20 @@
 package br.com.sancrisxa.cryptography.service;
 
-public class CryptoService {
+import org.jasypt.util.text.StrongTextEncryptor;
 
+public class CryptoService {
+    private static final StrongTextEncryptor encryptor;
+
+    static {
+        encryptor = new StrongTextEncryptor();
+        encryptor.setPassword(System.getenv("APP_KEY"));
+    }
+
+    public static String encrypt(String rawText) {
+        return encryptor.encrypt(rawText);
+    }
+
+    public static String descrypt(String encryptedText) {
+        return encryptor.decrypt(encryptedText);
+    }
 }
